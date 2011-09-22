56d0bd6d0649ee8b122a28e5232f715968de355f
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-09-22 11:32:01 +0000

    update some testcode

---
+++ trunk/zkbindtest/src/org/zkoss/zkbind/unitest/AllTests.java
@@ -0,0 +1,14 @@
+package org.zkoss.zkbind.unitest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+@RunWith(Suite.class)
+@SuiteClasses( { LoadTestCase.class})
+public class AllTests {
+
+}
