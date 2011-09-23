05a296cc05b4babd6d7376bf1667c94e8e87da92
chenhenri@gmail.com <chenhenri@gmail.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-09-23 08:06:02 +0000

    Add disabled button to try binding with InputElement instant mode

---
+++ zkbinddemo/src/org/zkoss/mvvm/examples/disabled/DisabledButtonViewModel.java
@@ -0,0 +1,50 @@
+/* DisabledButtonViewModel.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Sep 21, 2011 11:54:49 AM, Created by henrichen
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+*/
+
+package org.zkoss.mvvm.examples.disabled;
+
+import org.zkoss.zk.ui.event.InputEvent;
+import org.zkoss.zkbind.BindContext;
+import org.zkoss.zkbind.GenericBindComposer;
+import org.zkoss.zkbind.NotifyChange;
+
+/**
+ * 1. whenever the textbox is empty, the button is disabled;
+ * 2. whenever the textbox is not empty, the button is enabled.
+ * 
+ * @author henrichen
+ *
+ */
+public class DisabledButtonViewModel extends GenericBindComposer {
+	private String symbol;
+	private String lastSymbol;
+
+	//properties//
+	public String getSymbol() {
+		return this.symbol;
+	}
+
+	@NotifyChange
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	
+	public String getLastSymbol() {
+		return this.lastSymbol;
+	}
+
+	//commands//
+	@NotifyChange("lastSymbol")
+	public void subscribe() {
+		this.lastSymbol = this.symbol;
+	}
+}
