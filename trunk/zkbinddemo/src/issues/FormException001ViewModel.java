489d08913b6998363276d696234d1717ed838641
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-10-03 11:12:09 +0000

    exception when evaluating form getValueReference

---
+++ trunk/zkbinddemo/src/issues/FormException001ViewModel.java
@@ -0,0 +1,79 @@
+/* ConfirmViewModel.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Sep 21, 2011 11:54:49 AM, Created by DennisChen
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+ */
+
+package issues;
+
+import java.util.Set;
+
+import org.zkoss.zkbind.GenericBindComposer;
+import org.zkoss.zkbind.NotifyChange;
+import org.zkoss.zkbind.Property;
+
+/**
+ * Demo how to use validation to do the confirmation.
+ */
+public class FormException001ViewModel extends GenericBindComposer {
+	private City selected;
+
+	public FormException001ViewModel() {
+		selected = new City("New York",30);
+	}
+
+	public City getSelected() {
+		return selected;
+	}
+
+	
+	public boolean validate(String cmd, Set<Property> properties, org.zkoss.zkbind.BindContext ctx){
+		return true;
+	}
+
+
+	//command
+	
+	@NotifyChange({"selected"})
+	public void update(){
+
+	}
+
+	public static class City {
+		String name;
+		int population;
+
+		public City(String name,int population) {
+			this.name = name;
+			this.population = population;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getPopulation() {
+			return population;
+		}
+
+		public void setPopulation(int population) {
+			this.population = population;
+		}
+		
+		public String toString(){
+			return new StringBuilder().append("[").append(name).append(",").append(population).append("]").toString();
+		}
+		
+	}
+
+}
