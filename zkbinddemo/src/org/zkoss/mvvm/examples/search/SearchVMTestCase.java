881a6e0174bc632f82233d4f156438a02c144674
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-10-27 11:22:06 +0000

    example for smalltalk 1

---
+++ zkbinddemo/src/org/zkoss/mvvm/examples/search/SearchVMTestCase.java
@@ -0,0 +1,31 @@
+package org.zkoss.mvvm.examples.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchVMTestCase {
+	@Test
+	public void test01(){
+		SearchVM vm = new SearchVM();
+		
+		Assert.assertNull(vm.getItems());
+		vm.doSearch();
+		Assert.assertNotNull(vm.getItems());
+		Assert.assertEquals(20, vm.getItems().getSize());
+		
+		vm.setFilter("A");
+		vm.doSearch();
+		Assert.assertNotNull(vm.getItems());
+		Assert.assertEquals(4, vm.getItems().getSize());
+		
+		vm.setFilter("B");
+		vm.doSearch();
+		Assert.assertNotNull(vm.getItems());
+		Assert.assertEquals(4, vm.getItems().getSize());
+		
+		vm.setFilter("X");
+		vm.doSearch();
+		Assert.assertNotNull(vm.getItems());
+		Assert.assertEquals(0, vm.getItems().getSize());
+	}
+}
