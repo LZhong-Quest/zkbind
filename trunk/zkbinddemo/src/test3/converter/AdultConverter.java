adca6caee5c62d0a2c44de67d20a8e2d6567b798
hawkchen@potix.com <hawkchen@potix.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-10-28 09:41:35 +0000

    convert, collection test case, initial check in.

---
+++ trunk/zkbinddemo/src/test3/converter/AdultConverter.java
@@ -0,0 +1,22 @@
+package test3.converter;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.zk.ui.Component;
+
+public class AdultConverter implements Converter{
+
+	public Object coerceToUi(Object val, Component component, BindContext ctx) {
+		return val;
+	}
+
+	public Object coerceToBean(Object val, Component component, BindContext ctx) {
+		
+		Integer age = (Integer)val;
+		if (age >= 18){
+			return true;
+		}
+		return false;
+	}
+
+}
