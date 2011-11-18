36ce71c7b35a3f32391f5838fc54b582d69e2717
hawkchen@potix.com <hawkchen@potix.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-11-18 07:58:32 +0000

    id becomes auto generate by db.

--- trunk/springdemo/src/main/resources/import.sql
+++ trunk/springdemo/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into orders (id, description,price,quantity,creationDate) values ('01', 'part AF2 order',20,10,CURRENT_DATE)
-insert into orders (id, description,price,quantity,creationDate) values ('02', 'part BB2 order',35,10,CURRENT_DATE)
-insert into orders (id, description,price,quantity,creationDate) values ('03', 'part CX1 order',72,10,CURRENT_DATE)
-insert into orders (id, description,price,quantity,creationDate) values ('04', 'part DS34 order',88,10,CURRENT_DATE)
-insert into orders (id, description,price,quantity,creationDate) values ('05', 'part ZK99 order',85,10,CURRENT_DATE)
+insert into orders ( description,price,quantity,creationDate) values ( 'part AF2 order',20,10,CURRENT_DATE)
+insert into orders ( description,price,quantity,creationDate) values ( 'part BB2 order',35,10,CURRENT_DATE)
+insert into orders ( description,price,quantity,creationDate) values ( 'part CX1 order',72,10,CURRENT_DATE)
+insert into orders ( description,price,quantity,creationDate) values ( 'part DS34 order',88,10,CURRENT_DATE)
+insert into orders ( description,price,quantity,creationDate) values ( 'part ZK99 order',85,10,CURRENT_DATE)
 
