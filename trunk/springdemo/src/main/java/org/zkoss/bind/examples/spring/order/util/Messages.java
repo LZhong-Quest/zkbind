05f6fbd630a857cb3f89644d60ebfdcc194b2438
hawkchen@potix.com <hawkchen@potix.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-11-22 02:42:04 +0000

    remove comments & unused code.

--- trunk/springdemo/src/main/java/org/zkoss/bind/examples/spring/order/util/Messages.java
+++ trunk/springdemo/src/main/java/org/zkoss/bind/examples/spring/order/util/Messages.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Component;
 public class Messages extends HashMap<String, String> {
 
 	private static final long serialVersionUID = 1L;
-	private final String NOT_NULL_MESSAGE = "must not null";
-	private final String NOT_POSITIVE_MESSAGE = "must large than 0";
 	
 
 }
