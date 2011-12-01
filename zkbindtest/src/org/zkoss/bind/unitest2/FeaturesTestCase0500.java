c3e47f3a45b49e9a871ca30fa199997acf535d4e
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-12-01 06:08:34 +0000

    automation test for 633

--- zkbindtest/src/org/zkoss/bind/unitest2/FeaturesTestCase0500.java
+++ zkbindtest/src/org/zkoss/bind/unitest2/FeaturesTestCase0500.java
@@ -47,4 +47,49 @@ public class FeaturesTestCase0500 extends TestCaseBase{
 		Assert.assertEquals("F",findWidget("$t31").getValue());
 	}
 	
+	
+	@Test
+	public void f00633(){
+		navigate(getTestCaseUrl("/bind/issue/F00633.zul"));
+		
+		Assert.assertEquals("onCreate 1",findWidget("$l11").getValue());
+		Assert.assertEquals("onCreate 2",findWidget("$l12").getValue());
+		
+		findWidget("$btn1").click();
+		Assert.assertEquals("doCommand1",findWidget("$l11").getValue());
+		
+		findWidget("$btn2").click();
+		Assert.assertEquals("doCommand2",findWidget("$l11").getValue());
+		
+		findWidget("$btn3").click();
+		Assert.assertEquals("doCommand3 btn3 true",findWidget("$l11").getValue());
+		
+		findWidget("$btn4").click();
+		Assert.assertEquals("doCommand4 null null null btn4 true",findWidget("$l11").getValue());
+		
+		findWidget("$btn5").click();
+		Assert.assertEquals("doCommand5 99 true XYZ btn5 true",findWidget("$l11").getValue());
+		
+		findWidget("$btn6").click();
+		Assert.assertEquals("doCommand6 9 true ABCD btn6 true",findWidget("$l11").getValue());
+		
+		findWidget("$btn7").click();
+		Assert.assertEquals("doCommandX 9 true XYZ cmd7",findWidget("$l11").getValue());
+		
+		findWidget("$btn8").click();
+		Assert.assertEquals("doCommandX 22 true ABCD cmd8",findWidget("$l11").getValue());
+		
+		findWidget("$btn9").click();
+		Assert.assertEquals("doCommandX 9 false EFG cmd9",findWidget("$l11").getValue());
+		
+		findWidget("$btn10").click();
+		Assert.assertEquals("object is btn10",findWidget("$l12").getValue());
+		
+		findWidget("$btn11").click();
+		Assert.assertEquals("object is desktop",findWidget("$l12").getValue());
+		
+		findWidget("$btn12").click();
+		Assert.assertEquals("object is h11",findWidget("$l12").getValue());
+		
+	}
 }
