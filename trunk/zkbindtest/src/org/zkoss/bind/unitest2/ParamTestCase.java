b85bdf0037d6a2e9f7c1058f45f09f6911a34a72
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-12-16 10:50:29 +0000

    scope param test

--- trunk/zkbindtest/src/org/zkoss/bind/unitest2/ParamTestCase.java
+++ trunk/zkbindtest/src/org/zkoss/bind/unitest2/ParamTestCase.java
@@ -48,4 +48,61 @@ public class ParamTestCase extends TestCaseBase{
 		Assert.assertEquals("",findWidget("$w2 $l11").getValue());
 		Assert.assertEquals("",findWidget("$w2 $l12").getValue());
 	}
+	
+	@Test
+	public void testScopeParam(){
+		navigate(getTestCaseUrl("/bind/basic/scopeparam.zul"));
+		
+		Assert.assertEquals("applicationScope-A",findWidget("$l11").getValue());
+		Assert.assertEquals("sessionScope-A",findWidget("$l12").getValue());
+		Assert.assertEquals("desktopScope-A",findWidget("$l13").getValue());
+		Assert.assertEquals("spaceScopeScope-A",findWidget("$l14").getValue());
+		Assert.assertEquals("B",findWidget("$l15").getValue());
+		Assert.assertEquals("C",findWidget("$l16").getValue());
+		Assert.assertEquals("E",findWidget("$l17").getValue());
+		Assert.assertEquals("",findWidget("$l18").getValue());
+		
+		
+		findWidget("$cmd1").click();
+		Assert.assertEquals("applicationScope-A",findWidget("$l11").getValue());
+		Assert.assertEquals("sessionScope-A",findWidget("$l12").getValue());
+		Assert.assertEquals("desktopScope-A",findWidget("$l13").getValue());
+		Assert.assertEquals("spaceScopeScope-A",findWidget("$l14").getValue());
+		Assert.assertEquals("F",findWidget("$l15").getValue());
+		Assert.assertEquals("C",findWidget("$l16").getValue());
+		Assert.assertEquals("D",findWidget("$l17").getValue());
+		Assert.assertEquals("G",findWidget("$l18").getValue());
+		
+		
+		findWidget("$cmd2").click();
+		Assert.assertEquals("var2 by Desktop",findWidget("$l11").getValue());
+		Assert.assertEquals("var1 by Desktop",findWidget("$l12").getValue());
+		Assert.assertEquals("desktopScope-A",findWidget("$l13").getValue());
+		Assert.assertEquals("spaceScopeScope-A",findWidget("$l14").getValue());
+		Assert.assertEquals("F",findWidget("$l15").getValue());
+		Assert.assertEquals("C",findWidget("$l16").getValue());
+		Assert.assertEquals("D",findWidget("$l17").getValue());
+		Assert.assertEquals("G",findWidget("$l18").getValue());
+		
+		
+		findWidget("$cmd3").click();
+		Assert.assertEquals("applicationScope-A",findWidget("$l11").getValue());
+		Assert.assertEquals("sessionScope-A",findWidget("$l12").getValue());
+		Assert.assertEquals("desktopScope-A",findWidget("$l13").getValue());
+		Assert.assertEquals("spaceScopeScope-A",findWidget("$l14").getValue());
+		Assert.assertEquals("F",findWidget("$l15").getValue());
+		Assert.assertEquals("C",findWidget("$l16").getValue());
+		Assert.assertEquals("D",findWidget("$l17").getValue());
+		Assert.assertEquals("G",findWidget("$l18").getValue());
+		
+		findWidget("$cmd2").click();
+		Assert.assertEquals("var2 by Desktop",findWidget("$l11").getValue());
+		Assert.assertEquals("var1 by Desktop",findWidget("$l12").getValue());
+		Assert.assertEquals("desktopScope-A",findWidget("$l13").getValue());
+		Assert.assertEquals("spaceScopeScope-A",findWidget("$l14").getValue());
+		Assert.assertEquals("F",findWidget("$l15").getValue());
+		Assert.assertEquals("C",findWidget("$l16").getValue());
+		Assert.assertEquals("D",findWidget("$l17").getValue());
+		Assert.assertEquals("G",findWidget("$l18").getValue());
+	}
 }
