2e379f9b7be2a45cac510ad59bc2810cf7171928
hawkchen@potix.com <hawkchen@potix.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2011-12-29 06:28:56 +0000

    Branch for keeping source code consistent with small talk - form binding written in 2011/12. Because zkbind specification keeps changing, example code will use new validation syntax.

--- springdemo/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into orders ( description,price,quantity,creationDate) values ( 'part AF2 order',20,10,CURRENT_DATE)
-insert into orders ( description,price,quantity,creationDate) values ( 'part BB2 order',35,10,CURRENT_DATE)
-insert into orders ( description,price,quantity,creationDate) values ( 'part CX1 order',72,10,CURRENT_DATE)
-insert into orders ( description,price,quantity,creationDate) values ( 'part DS34 order',88,10,CURRENT_DATE)
-insert into orders ( description,price,quantity,creationDate) values ( 'part ZK99 order',85,10,CURRENT_DATE)
-
