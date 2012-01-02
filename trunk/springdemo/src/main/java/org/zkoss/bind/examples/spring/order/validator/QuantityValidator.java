2e90210b999a23487233daeb2e773e67fc675cd1
atticcat <atticcat@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2012-01-02 03:11:56 +0000

    refine for new validation and zk wire variable spec

--- trunk/springdemo/src/main/java/org/zkoss/bind/examples/spring/order/validator/QuantityValidator.java
+++ trunk/springdemo/src/main/java/org/zkoss/bind/examples/spring/order/validator/QuantityValidator.java
@@ -1,26 +1,18 @@
 package org.zkoss.bind.examples.spring.order.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.zkoss.bind.ValidationContext;
-import org.zkoss.bind.Validator;
-import org.zkoss.bind.examples.spring.order.util.Messages;
+import org.zkoss.bind.validator.AbstractValidator;
 
 @Component("quantityValidator")
 @Scope("prototype")
-public class QuantityValidator implements Validator{
-	@Autowired
-	Messages messages;
+public class QuantityValidator extends AbstractValidator{
+
 	public void validate(ValidationContext ctx) {
 		Integer quantity = (Integer)ctx.getProperty().getValue();
 		if(quantity==null || quantity<=0){
-			ctx.setInvalid();// mark invalid
-			messages.put("quantity", "must large than 0");
-		}else{
-			messages.remove("quantity");
+			addInvalidMessage(ctx, "must large than 0");
 		}
-		//notify messages was changed.
-		ctx.getBindContext().getBinder().notifyChange(messages, "quantity");
 	}
 }
