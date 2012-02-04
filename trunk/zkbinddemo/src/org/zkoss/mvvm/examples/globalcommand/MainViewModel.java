28915f0a7c07889d4fda60c3bee4a418f7d36fe4
hawkchen@potix.com <hawkchen@potix.com@d17ffec1-e06f-2ac7-8d19-2f512a2d47ba>
2012-02-04 05:58:56 +0000

    rename class: MainViewModel -> MasterViewModel AddViewModel -> MainViewModel

--- trunk/zkbinddemo/src/org/zkoss/mvvm/examples/globalcommand/MainViewModel.java
+++ trunk/zkbinddemo/src/org/zkoss/mvvm/examples/globalcommand/MainViewModel.java
@@ -1,8 +1,5 @@
 package org.zkoss.mvvm.examples.globalcommand;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.zkoss.bind.ValidationContext;
 import org.zkoss.bind.Validator;
 import org.zkoss.bind.annotation.Command;
@@ -13,43 +10,20 @@ import org.zkoss.bind.validator.AbstractValidator;
 public class MainViewModel {
 
 	private boolean visible = true;
-	
-	private String[] names ={"Peter", "John","Matthew", "Simon", "Lucas"};
-
-	private String selected;
-	private int number;
-	
+	private String item;
 	private String msg;
 	
-	
-	public List<String> getNames() {
-		return Arrays.asList(names);
-	}
-
 	public boolean isVisible() {
 		return visible;
 	}
-	
 
 	@Command @NotifyChange("msg")
-	public void select(){
-		msg = "You select "+selected;
+	public void add(){
+		ItemList.add(item);
+		msg = "Added "+item;
 	}
 	
-	@Command @NotifyChange("msg")
-	public void submit(){
-		selected = names[number];
-		msg = "You select "+selected;
-	}	
-
-	public String getSelected() {
-		return selected;
-	}
-
-	public void setSelected(String selected) {
-		this.selected = selected;
-	}
-
+	
 	public String getMsg() {
 		return msg;
 	}
@@ -67,26 +41,29 @@ public class MainViewModel {
 		visible =false;
 	}
 
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
-	}
 	
-	public Validator getStaffNumberValidator(){
+	public Validator getItemValidator(){
 		return new AbstractValidator() {
 			
 			public void validate(ValidationContext ctx) {
-				if (ctx.getProperty().getValue() instanceof Integer){
-					Integer staffIndex =(Integer)ctx.getProperty().getValue()-1;
-					if (staffIndex> names.length || staffIndex <0){
-						addInvalidMessage(ctx, "Number out of range.");
+				if (ctx.getProperty().getValue() instanceof String){
+					String item = (String)ctx.getProperty().getValue();
+					if (null == item || item.length()<=2){
+						addInvalidMessage(ctx, "Too short item name");
 					}
 				}
 				
 			}
 		};
+	}
+
+
+	public String getItem() {
+		return item;
+	}
+
+
+	public void setItem(String item) {
+		this.item = item;
 	}
 }
